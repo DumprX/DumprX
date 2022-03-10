9eabfeaf4558e404687e1ee9ac94d5ed7cfdf9f8
Sushrut1101 <guptasushrut@gmail.com>
2022-03-10 19:56:31 +0530

    Do Not ask for Confirmation for apt
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ "$(command -v apt)" != "" ]]; then
 	sudo apt update
-        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev jq
+        sudo apt install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev jq
     elif [[ "$(command -v pacman)" != "" ]]; then
 	sudo pacman -Syy --noconfirm
         sudo pacman -Sy --noconfirm unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4 jq
